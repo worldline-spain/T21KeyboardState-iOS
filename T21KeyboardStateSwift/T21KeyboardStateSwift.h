197feb322339b6701f6a8c4293e1bc50a0a3ff96
Anan Sadiya <anan.sadiya@worldline.com>
2019-06-14 09:54:52 +0200

    First approach Carthage integration

---
+++ T21KeyboardStateSwift/T21KeyboardStateSwift.h
@@ -0,0 +1,19 @@
+//
+//  T21KeyboardStateSwift.h
+//  T21KeyboardStateSwift
+//
+//  Created by Anan Sadiya on 14/06/2019.
+//  Copyright © 2019 Worldline. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for T21KeyboardStateSwift.
+FOUNDATION_EXPORT double T21KeyboardStateSwiftVersionNumber;
+
+//! Project version string for T21KeyboardStateSwift.
+FOUNDATION_EXPORT const unsigned char T21KeyboardStateSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <T21KeyboardStateSwift/PublicHeader.h>
+
+
